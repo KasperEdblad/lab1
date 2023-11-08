f77511a231a586cd607f67c1cec2f0797f2c3ae9
KasperEdblad <kasper.edblad@gmail.com>
2023-11-08 10:46:10 +0100

    Upgift 1

---
+++ Car.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class Car {
+
+    public int nrDoors;
+    public double enginePower;
+    public double currentSpeed;
+    private Color color;
+    public String modelName;
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+    public Color getColor(){
+        return color;
+    }
+    public void setColor(Color clr){
+        color = clr;
+    }
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+
+}
